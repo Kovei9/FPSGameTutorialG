22e99ca70d8235bcc5444d54105139b2d2295390
Mackenzie Burt <156246572+KenzieBurt@users.noreply.github.com>
2024-02-05 13:39:28 -0700

    Game
    Adding files

---
+++ Individual Game Project Early Stages/Assets/Spin.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    public class Spin : MonoBehaviour {
+    public float speed = 6.0f;
+    void Update() {
+        transform.Translate(0, speed, 0);
+    }
+}
